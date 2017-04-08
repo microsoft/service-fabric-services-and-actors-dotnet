98b9921bbc5a3d781ec7fba911685e5494417e85
Stephen Price <stephen@lythixdesigns.com>
2017-04-08 09:02:02 +0800

    Renamed test project to follow roslyn project-system naming style. Added two new unit test projects with a couple of simple tests.

---
+++ src/Microsoft.ServiceFabric.Services.Remoting.Tests/RemoteExceptionInformationTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Microsoft.ServiceFabric.Services.Remoting.Tests
+{
+    public class RemoteExceptionInformationTests
+    {
+        [Fact]
+        public void FromExceptionTest()
+        {
+            // Arrange
+            var expectedLength = 944;
+            Exception exception = new Exception("Test Exception");
+
+            // Act
+            var result = RemoteExceptionInformation.FromException(exception);
+
+            // Assert
+            Assert.Equal(expectedLength, result.Data.Length);
+        }
+    }
+}
---
+++ src/Microsoft.ServiceFabric.Services.Tests/ServiceNameFormatTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Microsoft.ServiceFabric.Services.Tests
+{
+    public class ServiceNameFormatTests
+    {
+        [Fact]
+        public void GetServiceNameFormat_NoServiceNameProvided_ReturnEndpointName()
+        {
+            // Arrange
+            string serviceName = "ObjectServiceEndpoint";
+
+            // Act
+            var result = ServiceNameFormat.GetEndpointName(typeof(object));
+
+            // Assert
+            Assert.Equal(serviceName, result);
+        }
+    }
+}
