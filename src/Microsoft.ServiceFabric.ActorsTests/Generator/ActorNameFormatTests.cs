7cc2c4f46f2728a7714ca439920e3b6eaf62a5d1
Stephen Price <stephen@lythixdesigns.com>
2017-04-08 08:21:30 +0800

    Added initial test project to get the ball rolling and establish baseline tools

---
+++ src/Microsoft.ServiceFabric.ActorsTests/Generator/ActorNameFormatTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.ServiceFabric.Actors.Generator;
+using Xunit;
+
+namespace Microsoft.ServiceFabric.ActorsTests.Generator
+{
+    public class ActorNameFormatTests
+    {
+        [Fact]
+        public void GetFabricService_NoServiceNameProvided_ReturnServiceName()
+        {
+            // Arrange
+            string serviceName = "ObjectActorService";
+
+            // Act
+            var result = ActorNameFormat.GetFabricServiceName(typeof(object)); 
+
+            // Assert
+            Assert.Equal(serviceName, result);
+        }
+
+        [Fact]
+        public void GetFabricService_PassServiceName_ReturnServiceName()
+        {
+            // Arrange
+            string serviceName = "serviceName";
+
+            // Act
+            var result = ActorNameFormat.GetFabricServiceName(typeof(object), serviceName); 
+
+            // Assert
+            Assert.Equal(serviceName, result);
+        }
+    }
+}
