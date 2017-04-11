3e93da488635849e6698677208bf80a06d9841a8
Stephen Price <stephen@lythixdesigns.com>
2017-04-11 11:54:33 +0800

    Created three xUnit Unit Test projects to get the ball rolling (#4)
    Added initial test project to get the ball rolling and establish baseline tools

---
+++ src/test/unittests/Microsoft.ServiceFabric.Services.Remoting.Tests/RemoteExceptionInformationTests.cs
@@ -0,0 +1,26 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+namespace Microsoft.ServiceFabric.Services.Remoting.Tests
+{
+    using System;
+    using Xunit;
+
+    public class RemoteExceptionInformationTests
+    {
+        [Fact]
+        public void FromExceptionTest()
+        {
+            // Arrange
+            var expectedLength = 944;
+            Exception exception = new Exception("Test Exception");
+
+            // Act
+            var result = RemoteExceptionInformation.FromException(exception);
+
+            // Assert
+            Assert.Equal(expectedLength, result.Data.Length);
+        }
+    }
+}
---
+++ src/test/unittests/Microsoft.ServiceFabric.Services.Tests/ServiceNameFormatTests.cs
@@ -0,0 +1,24 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+namespace Microsoft.ServiceFabric.Services.Tests
+{
+    using Xunit;
+
+    public class ServiceNameFormatTests
+    {
+        [Fact]
+        public void GetServiceNameFormat_NoServiceNameProvided_ReturnEndpointName()
+        {
+            // Arrange
+            string serviceName = "ObjectServiceEndpoint";
+
+            // Act
+            var result = ServiceNameFormat.GetEndpointName(typeof(object));
+
+            // Assert
+            Assert.Equal(serviceName, result);
+        }
+    }
+}
