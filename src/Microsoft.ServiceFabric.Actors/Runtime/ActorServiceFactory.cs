819179a12ce42b561fb8d9e5f389eeab29a4dc25
Grahame Horner <grahame@gabriel-systems.net>
2017-08-11 16:52:15 +0100

    updated actor runtime and actor service factory to allow custom IActorActivator to be supplied

--- src/Microsoft.ServiceFabric.Actors/Runtime/ActorServiceFactory.cs
+++ src/Microsoft.ServiceFabric.Actors/Runtime/ActorServiceFactory.cs
@@ -14,23 +14,46 @@ namespace Microsoft.ServiceFabric.Actors.Runtime
         private readonly ActorTypeInformation actorTypeInformation;
         private readonly ActorMethodDispatcherMap methodDispatcherMap;
         private readonly ActorMethodFriendlyNameBuilder methodFriendlyNameBuilder;
-        private readonly Func<StatefulServiceContext, ActorTypeInformation, ActorService> actorServiceFactory;
 
+        private readonly Func<StatefulServiceContext,
+            ActorTypeInformation,
+            Func<
+                Func<ActorService,
+                    ActorId,
+                    ActorBase>,
+                IActorActivator>,
+            ActorService> actorServiceFactory;
+        
+        private readonly Func<
+            Func<ActorService,
+                ActorId,
+                ActorBase>,
+            IActorActivator> actorActivatorFactory;
+        
         public ActorServiceFactory(
             ActorTypeInformation actorTypeInformation,
             ActorMethodFriendlyNameBuilder methodFriendlyNameBuilder,
-            Func<StatefulServiceContext, ActorTypeInformation, ActorService> actorServiceFactory,
-            ActorMethodDispatcherMap methodDispatcherMap = null)
+            Func<StatefulServiceContext, 
+                 ActorTypeInformation,
+                 Func<Func<ActorService,ActorId,ActorBase>,IActorActivator>,
+                 ActorService> actorServiceFactory,
+            ActorMethodDispatcherMap methodDispatcherMap = null,
+            Func<Func<ActorService,ActorId,ActorBase>,IActorActivator> actorActivatorFactory = null)
         {
             this.actorTypeInformation = actorTypeInformation;
             this.methodDispatcherMap = methodDispatcherMap;
             this.methodFriendlyNameBuilder = methodFriendlyNameBuilder;
             this.actorServiceFactory = actorServiceFactory;
+            this.actorActivatorFactory = actorActivatorFactory;
         }
 
         public ActorService CreateActorService(StatefulServiceContext context)
         {
-            var serviceReplica = this.actorServiceFactory.Invoke(context, this.actorTypeInformation);
+            var serviceReplica = this.actorServiceFactory
+                .Invoke(context, 
+                    this.actorTypeInformation,
+                    this.actorActivatorFactory
+                );
 
             // Initialize here so that service can set function in constructor.
             serviceReplica.StateProvider.Initialize(this.actorTypeInformation);
