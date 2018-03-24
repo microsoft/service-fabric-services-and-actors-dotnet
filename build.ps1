768de51dd116e9182a2d51ef34f30d1e8dd71046
Aman Bhardwaj <amanbha@microsoft.com>
2018-03-24 16:06:47 -0700

    Updating build script and pdb info

--- build.ps1
+++ build.ps1
@@ -5,51 +5,80 @@
 param
 (
     # Configuration to build.
-	[ValidateSet('Debug', 'Release')]
-	[string]$configuration = "Release",	 
-	
-    # target to build. 
-	#Options are:
-	#   RebuildAll: Clean, Build all .csproj and Generate Nuget Packages. This is the default option.
-	#   BuildAll: Build all .csproj and Generate Nuget Packages.
-	#   GeneratePackages: Build all product .csprojs and generate nuget packages.
-	#   BuildTests: Builds all test .csprojs.
-	#   BuildProduct: Builds all product .csprojs.
-	[ValidateSet('Rebuildall', 'BuildAll', 'GeneratePackages')]
-	[string]$target = "RebuildAll",
+    [ValidateSet('Debug', 'Release')]
+    [string]$Configuration = "Release",
 
-	# msbuild verbosity level.
-	[ValidateSet('quiet','minimal', 'normal', 'detailed', 'diagnostic')]
-	[string]$verbosity = 'minimal'
+    # target to build. 
+    #Options are:
+    #   RebuildAll: Clean, Build all .csproj and Generate Nuget Packages. This is the default option.
+    #   BuildAll: Build all .csproj and Generate Nuget Packages.
+    #   GeneratePackages: Build all product .csprojs and generate nuget packages.
+    #   BuildTests: Builds all test .csprojs.
+    #   BuildProduct: Builds all product .csprojs.
+    [ValidateSet('Rebuildall', 'BuildAll', 'GeneratePackages')]
+    [string]$Target = "RebuildAll",
+
+    # msbuild verbosity level.
+    [ValidateSet('quiet','minimal', 'normal', 'detailed', 'diagnostic')]
+    [string]$Verbosity = 'minimal',
+
+    # path to msbuild
+    [string]$MSBuildFullPath
 )
 
-# Check msbuild exists. Find msbuild for VS2017
-if (Test-Path "env:\ProgramFiles(x86)") 
+if($MSBuildFullPath -ne "")
 {
-	$progFilesPath =  ${env:ProgramFiles(x86)}
-}
-elseif (Test-Path "env:\ProgramFiles") 
-{
-	$progFilesPath =  ${env:ProgramFiles}
+    if (!(Test-Path $MSBuildFullPath))
+    {
+        throw "Unable to find MSBuild at the specified path, run the script again with correct path to msbuild."
+    }
 }
 
-$VS2017InstallPath = join-path $progFilesPath "Microsoft Visual Studio\2017"
-$versions = 'Community', 'Professional', 'Enterprise'
-
-foreach ($version in $versions)
+# msbuild path not provided, find msbuild for VS2017
+if($MSBuildFullPath -eq "")
 {
-	$VS2017VersionPath = join-path $VS2017InstallPath $version
-	$msbuildFullPath = join-path $VS2017VersionPath "MSBuild\15.0\Bin\MSBuild.exe"
+    if (Test-Path "env:\ProgramFiles(x86)")
+    {
+        $progFilesPath =  ${env:ProgramFiles(x86)}
+    }
+    elseif (Test-Path "env:\ProgramFiles")
+    {
+        $progFilesPath =  ${env:ProgramFiles}
+    }
 
-	if (Test-Path $msbuildFullPath)
-	{
-		break
-	}
+    $VS2017InstallPath = join-path $progFilesPath "Microsoft Visual Studio\2017"
+    $versions = 'Community', 'Professional', 'Enterprise'
+
+    foreach ($version in $versions)
+    {
+        $VS2017VersionPath = join-path $VS2017InstallPath $version
+        $MSBuildFullPath = join-path $VS2017VersionPath "MSBuild\15.0\Bin\MSBuild.exe"
+
+        if (Test-Path $MSBuildFullPath)
+        {
+            break
+        }
+    }
+
+    if (!(Test-Path $MSBuildFullPath))
+    {
+        Write-Host "Visual Studio 2017 installation not found in ProgramFiles, trying to find install path from registry."
+        if(Test-Path -Path HKLM:\SOFTWARE\WOW6432Node)
+        {
+            $VS2017VersionPath = Get-ItemProperty (Get-ItemProperty -Path HKLM:\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7 -Name "15.0")."15.0"
+        }
+        else
+        {
+            $VS2017VersionPath = Get-ItemProperty (Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\VisualStudio\SxS\VS7 -Name "15.0")."15.0"
+        }
+
+        $MSBuildFullPath = join-path $VS2017VersionPath "MSBuild\15.0\Bin\MSBuild.exe"
+    }
 }
 
-if (!(Test-Path $msbuildFullPath))
+if (!(Test-Path $MSBuildFullPath))
 {
-	throw "Unable to find MSBuild installed on this machine. Please install it (or install Visual Studio 2017.)"
+    throw "Unable to find MSBuild installed on this machine. Please install Visual Studio 2017 or if its installed at non-default location, provide the full ppath to msbuild using -MSBuildFullPath parameter."
 }
 
 
