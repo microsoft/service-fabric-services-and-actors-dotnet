23ecd13d10280ebc9fef356d67d21464167262a1
Aman Bhardwaj <amanbha@microsoft.com>
2018-06-06 20:06:40 -0700

    Update copyright header

--- src/Microsoft.ServiceFabric.Actors.Wcf/_friend.cs
+++ src/Microsoft.ServiceFabric.Actors.Wcf/_friend.cs
@@ -1,4 +1,4 @@
 // ------------------------------------------------------------
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT License (MIT).See License.txt in the repo root for license information.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
 // ------------------------------------------------------------
