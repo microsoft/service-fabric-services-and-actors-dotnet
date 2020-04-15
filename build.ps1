b9a08ff7bff165ad2776848aa01c437bc7feb675
Aman Bhardwaj <amanbha@microsoft.com>
2020-04-15 10:43:17 -0700

    Use github actions (#242)
    Adding github actions workflow, updating tests and build scripts for it.
    
    (cherry picked from commit 7dd37f6d65743ec3b15ec04283c6aaacdcf5091b)

--- build.ps1
+++ build.ps1
@@ -19,54 +19,8 @@ param
 
     # msbuild verbosity level.
     [ValidateSet('quiet','minimal', 'normal', 'detailed', 'diagnostic')]
-    [string]$Verbosity = 'minimal',
-
-    # path to msbuild
-    [string]$MSBuildFullPath
+    [string]$Verbosity = 'minimal'
 )
 
-if($MSBuildFullPath -ne "")
-{
-    if (!(Test-Path $MSBuildFullPath))
-    {
-        throw "Unable to find MSBuild at the specified path, run the script again with correct path to msbuild."
-    }
-}
-
-# msbuild path not provided, find msbuild for VS2017
-if($MSBuildFullPath -eq "")
-{
-    if (Test-Path "env:\ProgramFiles(x86)")
-    {
-        $progFilesPath =  ${env:ProgramFiles(x86)}
-    }
-    elseif (Test-Path "env:\ProgramFiles")
-    {
-        $progFilesPath =  ${env:ProgramFiles}
-    }
-
-    $VS2019InstallPath = join-path $progFilesPath "Microsoft Visual Studio\2019"
-    $versions = 'Community', 'Professional', 'Enterprise'
-
-    foreach ($version in $versions)
-    {
-        $VS2019VersionPath = join-path $VS2019InstallPath $version
-        $MSBuildFullPath = join-path $VS2019VersionPath "MSBuild\Current\Bin\MSBuild.exe"
-
-        if (Test-Path $MSBuildFullPath)
-        {
-            break
-        }
-    }
-}
-
-if (!(Test-Path $MSBuildFullPath))
-{
-    throw "Unable to find MSBuild installed on this machine. Please install Visual Studio 2019 or if its installed at non-default location, provide the full path to msbuild using -MSBuildFullPath parameter."
-}
-
-
-Write-Output "Using msbuild from $msbuildFullPath"
-
 $msbuildArgs = @("buildall.proj", "/nr:false", "/nologo", "/t:$target", "/verbosity:$verbosity", "/property:RequestedVerbosity=$verbosity", "/property:Configuration=$configuration", $args)
-& $msbuildFullPath $msbuildArgs
+dotnet msbuild $msbuildArgs
