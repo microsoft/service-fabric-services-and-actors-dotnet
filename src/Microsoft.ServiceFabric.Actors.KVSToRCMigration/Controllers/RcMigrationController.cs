99f9a5a4cdd9fcb3946bed9ef6d93e9a0b96a342
nehal6657 <nehalreshuaaryan@gmail.com>
2022-08-04 15:09:49 +0530

    added headers to resolve cors issue in angular app

--- src/Microsoft.ServiceFabric.Actors.KVSToRCMigration/Controllers/RcMigrationController.cs
+++ src/Microsoft.ServiceFabric.Actors.KVSToRCMigration/Controllers/RcMigrationController.cs
@@ -8,6 +8,7 @@ namespace Microsoft.ServiceFabric.Actors.KVSToRCMigration.Controllers
     using System;
     using System.Threading;
     using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Cors;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.ServiceFabric.Actors.Migration;
     using Microsoft.ServiceFabric.Actors.Runtime.Migration;
