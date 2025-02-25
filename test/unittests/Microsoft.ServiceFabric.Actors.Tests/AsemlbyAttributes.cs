7b2309a82ca26003de4ea35c5bfb85940ebbeec4
Pavle Ciric <plaveongit@outlook.com>
2025-02-25 14:35:28 +0100

    Refactor assembly attributes for test projects

--- test/unittests/Microsoft.ServiceFabric.Actors.Tests/AsemlbyAttributes.cs
+++
@@ -1,5 +0,0 @@
-using Xunit;
-
-// This attribute is used to run the tests in the same assembly in sequence.
-// It is necessary for ActorRemotingProviderAttributeTest suit to run properly.
-[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly)]
