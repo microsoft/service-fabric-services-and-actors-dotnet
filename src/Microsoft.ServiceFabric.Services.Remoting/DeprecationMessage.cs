17bc8cbe39a2cb8bcafb9b75c851b89565f87d59
Pavle Ciric <plaveongit@outlook.com>
2025-02-25 15:13:53 +0100

    Add info to RemotingV1 deprecation message
    More information about the deprecation of Service Remoting V1 can be found on the link.

--- src/Microsoft.ServiceFabric.Services.Remoting/DeprecationMessage.cs
+++ src/Microsoft.ServiceFabric.Services.Remoting/DeprecationMessage.cs
@@ -8,6 +8,7 @@ namespace Microsoft.ServiceFabric.Services.Remoting
 {
     internal static class DeprecationMessage
     {
-        public const string RemotingV1 = "Service Remoting V1 is deprecated. Please use Service Remoting V2 instead.";
+        public const string RemotingV1 = "Service Remoting V1 is deprecated. Please use Service Remoting V2 instead. " +
+            "Please refer to the release notes for more details: https://github.com/microsoft/service-fabric/blob/master/release_notes/Deprecated/RemotingV1.md";
     }
 }
