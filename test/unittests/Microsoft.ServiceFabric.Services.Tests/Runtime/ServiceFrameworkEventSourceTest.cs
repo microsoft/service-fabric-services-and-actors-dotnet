a45b1437630ee3fc3a1b9784c9da27ed00b91c90
Oleg Sych <olegsych@users.noreply.github.com>
2025-05-21 17:43:01 -0700

    Create initial tests for guids and manifests of the Actors and Services EventSource classes

---
+++ test/unittests/Microsoft.ServiceFabric.Services.Tests/Runtime/ServiceFrameworkEventSourceTest.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics.Tracing;
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.ServiceFabric.Services.Runtime
+{
+    public abstract class ServiceFrameworkEventSourceTest
+    {
+        readonly ServiceFrameworkEventSource sut = new ServiceFrameworkEventSource();
+
+        public sealed class Guid : ServiceFrameworkEventSourceTest
+        {
+            [Fact]
+            public void RemainsUnchangedForBackwardCompatibilityWithCollectionTools()
+            {
+                Assert.Equal(new System.Guid("13c2a97d-71da-5ab5-47cb-1497aec602e1"), new ServiceFrameworkEventSource().Guid);
+            }
+        }
+
+        public sealed class Manifest : ServiceFrameworkEventSourceTest
+        {
+            readonly ITestOutputHelper output;
+
+            public Manifest(ITestOutputHelper output) => this.output = output;
+
+            [Fact]
+            public void CanBeSavedForRegistrationWithExternalTools()
+            {
+                string manifest = EventSource.GenerateManifest(sut.GetType(), sut.GetType().Assembly.Location);
+                string manifestFile = Path.ChangeExtension(Path.Combine(Path.GetDirectoryName(sut.GetType().Assembly.Location), sut.Name), "man");
+                File.WriteAllText(manifestFile, manifest);
+                output.WriteLine("To register generated manifest for ETL tools, run");
+                output.WriteLine($"sudo wevutil install-manifest {manifestFile}");
+            }
+        }
+    }
+}
