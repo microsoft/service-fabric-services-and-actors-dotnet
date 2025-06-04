eda46f804acde5a39044f2898134ff72f70caf87
Ilija Grbić <ilijagrbi@microsoft.com>
2025-06-04 10:49:06 +0200

    Remove not needed dependencies

--- test/unittests/Microsoft.ServiceFabric.Diagnostics.Tests/TestServiceFabricEventSource.cs
+++ test/unittests/Microsoft.ServiceFabric.Diagnostics.Tests/TestServiceFabricEventSource.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 using Microsoft.ServiceFabric.Diagnostics.Tracing;
 using Microsoft.ServiceFabric.Diagnostics.Tracing.Config;
 using Microsoft.ServiceFabric.Diagnostics.Tracing.Writer;
-using Moq;
 using Xunit;
 
 namespace Microsoft.ServiceFabric.Diagnostics.Tests
