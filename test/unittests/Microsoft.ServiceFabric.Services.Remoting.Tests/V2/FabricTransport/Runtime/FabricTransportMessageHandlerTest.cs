51ac79456b7a3452e48e6c55f1acf3114648a93c
Ilija Grbić <ilijagrbi@microsoft.com>
2025-07-16 15:47:10 +0200

    Refactor FabricTransportMessageHandler test

--- test/unittests/Microsoft.ServiceFabric.Services.Remoting.Tests/V2/FabricTransport/Runtime/FabricTransportMessageHandlerTest.cs
+++ test/unittests/Microsoft.ServiceFabric.Services.Remoting.Tests/V2/FabricTransport/Runtime/FabricTransportMessageHandlerTest.cs
@@ -23,13 +23,68 @@ namespace Microsoft.ServiceFabric.Services.Remoting.Tests.V2.FabricTransport.Run
 {
     public abstract class FabricTransportMessageHandlerTest
     {
+        readonly internal FabricTransportMessageHandler sut;
+
+        readonly internal IServiceRemotingMessageHandler mockRemotingMessageHandler;
+        readonly internal IServiceRemotingMessageSerializersManager mockSerializerManager;
+        readonly internal IDiagnosticsSource mockDiagnosticsSource;
+
+        readonly internal ExceptionSerializer exceptionSerializer = new ExceptionSerializer(
+            new IExceptionConvertor[] { new DefaultExceptionConvertor() },
+            new FabricTransportRemotingListenerSettings { RemotingExceptionDepth = 2 }
+        );
+        readonly internal Guid partitionId = Guid.NewGuid();
+        readonly internal long replicaOrInstanceId = 123L;
+
+        public FabricTransportMessageHandlerTest()
+        {
+            // Local variables for mocking dependencies
+            var mockRequestMessageBodySerializer = Mock.Of<IServiceRemotingRequestMessageBodySerializer>();
+            var mockResponseMessageBodySerializer = Mock.Of<IServiceRemotingResponseMessageBodySerializer>();
+
+            var mockRequestMessageHeaderSerializer = Mock.Of<IServiceRemotingRequestMessageHeader>();
+            Mock.Get(mockRequestMessageHeaderSerializer).Setup(m => m.InterfaceId).Returns(0);
+
+            var mockSerializationProvider = Mock.Of<IServiceRemotingMessageSerializationProvider>();
+            Mock.Get(mockSerializationProvider).Setup(p => p.CreateResponseMessageSerializer(It.IsAny<Type>(), It.IsAny<Type[]>(), It.IsAny<Type[]>()))
+                .Returns(mockResponseMessageBodySerializer);
+
+            var mockHeaderSerializer = Mock.Of<IServiceRemotingMessageHeaderSerializer>();
+            Mock.Get(mockHeaderSerializer).Setup(d => d.SerializeResponseHeader(It.IsAny<IServiceRemotingResponseMessageHeader>()))
+                .Returns(Mock.Of<IMessageHeader>());
+            Mock.Get(mockHeaderSerializer).Setup(d => d.DeserializeRequestHeaders(It.IsAny<IMessageHeader>()))
+                .Returns(mockRequestMessageHeaderSerializer);
+
+            // Mock needed dependencies
+            this.mockRemotingMessageHandler = Mock.Of<IServiceRemotingMessageHandler>();
+            Mock.Get(this.mockRemotingMessageHandler).Setup(m => m.HandleRequestResponseAsync(It.IsAny<IServiceRemotingRequestContext>(), It.IsAny<IServiceRemotingRequestMessage>()))
+                .Returns(Task.FromResult(Mock.Of<IServiceRemotingResponseMessage>()));
+
+            this.mockDiagnosticsSource = Mock.Of<IDiagnosticsSource>();
+
+            this.mockSerializerManager = Mock.Of<IServiceRemotingMessageSerializersManager>();
+            Mock.Get(this.mockSerializerManager).Setup(m => m.GetHeaderSerializer())
+                .Returns(mockHeaderSerializer);
+            Mock.Get(this.mockSerializerManager).Setup(m => m.GetRequestBodySerializer(It.IsAny<int>()))
+                .Returns(mockRequestMessageBodySerializer);
+            Mock.Get(this.mockSerializerManager).Setup(m => m.GetResponseBodySerializer(It.IsAny<int>()))
+                .Returns(mockResponseMessageBodySerializer);
+
+            // Initialize System Under Test (SUT)
+            this.sut = new FabricTransportMessageHandler(
+                this.mockRemotingMessageHandler,
+                this.mockSerializerManager,
+                exceptionSerializer,
+                partitionId,
+                replicaOrInstanceId);
+        }
+
         public class Constructor : FabricTransportMessageHandlerTest
         {
             [Fact]
-            public void FabricTransportMessageHandler_ShouldHave_DiagnosticsManagerField()
+            public void ShouldHave_DiagnosticsSourceField()
             {
                 var handlerType = typeof(FabricTransportMessageHandler);
-
                 var diagnosticsSourceField = handlerType
                     .GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public)
                     .FirstOrDefault(f => f.FieldType == typeof(IDiagnosticsSource));
@@ -39,110 +94,45 @@ namespace Microsoft.ServiceFabric.Services.Remoting.Tests.V2.FabricTransport.Run
             }
 
             [Fact]
-            public void FabricTransportMessageHandler_Constructor_ShouldInstantiate_DiagnosticsManager()
+            public void ShouldInstantiate_DiagnosticsManagerAsSource()
             {
-                var mockRemotingMessageHandler = Mock.Of<IServiceRemotingMessageHandler>();
-                var mockHeaderSerializer = Mock.Of<IServiceRemotingMessageHeaderSerializer>();
-                var mockSerializersManager = Mock.Of<ServiceRemotingMessageSerializersManager>();
-                ExceptionSerializer mockExceptionSerializer = new ExceptionSerializer(
-                    new IExceptionConvertor[] { new DefaultExceptionConvertor() },
-                    new FabricTransportRemotingListenerSettings { RemotingExceptionDepth = 2 }
-                );
-                var partitionId = Guid.NewGuid();
-                var replicaOrInstanceId = 123L;
-
-                var handler = new FabricTransportMessageHandler(
-                    mockRemotingMessageHandler,
-                    mockSerializersManager,
-                    mockExceptionSerializer,
-                    partitionId,
-                    replicaOrInstanceId);
-
                 var handlerType = typeof(FabricTransportMessageHandler);
-                var diagnosticsManagerField = handlerType
+                var diagnosticsSourceField = handlerType
                     .GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
                     .FirstOrDefault(f => f.FieldType == typeof(IDiagnosticsSource));
 
-                Assert.NotNull(diagnosticsManagerField);
-                var diagnosticsManagerInstance = diagnosticsManagerField.GetValue(handler);
-                Assert.NotNull(diagnosticsManagerInstance);
-                Assert.IsType<DiagnosticsManager>(diagnosticsManagerInstance);
+                Assert.NotNull(diagnosticsSourceField);
+                var diagnosticsSourceInstance = diagnosticsSourceField.GetValue(sut);
+                Assert.NotNull(diagnosticsSourceInstance);
+                Assert.IsType<DiagnosticsManager>(diagnosticsSourceInstance);
             }
         }            
         
         public class RequestReponse : FabricTransportMessageHandlerTest
         {
-            private IServiceRemotingMessageHandler mockRemotingMessageHandler;
-            private IServiceRemotingMessageSerializersManager mockSerializerManager;
-            private IDiagnosticsSource mockDiagnosticsSource;
 
-            private ExceptionSerializer exceptionSerializer = new ExceptionSerializer(
-                    new IExceptionConvertor[] { new DefaultExceptionConvertor() },
-                    new FabricTransportRemotingListenerSettings { RemotingExceptionDepth = 2 }
-                );
-            private Guid partitionId = Guid.NewGuid();
-            private long replicaOrInstanceId = 123L;
+            FabricTransportMessage fabricTransportMessage = new FabricTransportMessage(new FabricTransportRequestHeader(Mock.Of<Stream>()), new FabricTransportRequestBody(Mock.Of<Stream>()));
+            FabricTransportRequestContext requestContext = new FabricTransportRequestContext(null, null);
 
             public RequestReponse()
             {
-                // Local variables for mocking dependencies
-                var mockRequestMessageBodySerializer = Mock.Of<IServiceRemotingRequestMessageBodySerializer>();
-                var mockResponseMessageBodySerializer = Mock.Of<IServiceRemotingResponseMessageBodySerializer>();
-
-                var mockRequestMessageHeaderSerializer = Mock.Of<IServiceRemotingRequestMessageHeader>();
-                Mock.Get(mockRequestMessageHeaderSerializer).Setup(m => m.InterfaceId).Returns(0);
-
-                var mockSerializationProvider = Mock.Of<IServiceRemotingMessageSerializationProvider>();
-                Mock.Get(mockSerializationProvider).Setup(p => p.CreateResponseMessageSerializer(It.IsAny<Type>(), It.IsAny<Type[]>(), It.IsAny<Type[]>()))
-                    .Returns(mockResponseMessageBodySerializer);
-
-                var mockHeaderSerializer = Mock.Of<IServiceRemotingMessageHeaderSerializer>();
-                Mock.Get(mockHeaderSerializer).Setup(d => d.SerializeResponseHeader(It.IsAny<IServiceRemotingResponseMessageHeader>()))
-                    .Returns(Mock.Of<IMessageHeader>());
-                Mock.Get(mockHeaderSerializer).Setup(d => d.DeserializeRequestHeaders(It.IsAny<IMessageHeader>()))
-                    .Returns(mockRequestMessageHeaderSerializer);
-
-                this.mockRemotingMessageHandler = Mock.Of<IServiceRemotingMessageHandler>();
-                Mock.Get(this.mockRemotingMessageHandler).Setup(m => m.HandleRequestResponseAsync(It.IsAny<IServiceRemotingRequestContext>(), It.IsAny<IServiceRemotingRequestMessage>()))
-                    .Returns(Task.FromResult(Mock.Of<IServiceRemotingResponseMessage>()));   
-
-                this.mockDiagnosticsSource = Mock.Of<IDiagnosticsSource>();
-
-                this.mockSerializerManager = Mock.Of<IServiceRemotingMessageSerializersManager>();
-                Mock.Get(this.mockSerializerManager).Setup(m => m.GetHeaderSerializer())
-                    .Returns(mockHeaderSerializer);
-                Mock.Get(this.mockSerializerManager).Setup(m => m.GetRequestBodySerializer(It.IsAny<int>()))
-                    .Returns(mockRequestMessageBodySerializer);
-                Mock.Get(this.mockSerializerManager).Setup(m => m.GetResponseBodySerializer(It.IsAny<int>()))
-                    .Returns(mockResponseMessageBodySerializer);
-            }   
-    
-            [Fact]
-            public async Task RequestResponseAsync_ShouldCall_OnRequestResponseBeginEnd()
-            {
-                var fabricTransportMessage = new FabricTransportMessage(new FabricTransportRequestHeader(Mock.Of<Stream>()), new FabricTransportRequestBody(Mock.Of<Stream>()));
-                var requestContext = new FabricTransportRequestContext(null, null);
-
-                var handler = new FabricTransportMessageHandler(
-                    mockRemotingMessageHandler,
-                    mockSerializerManager,
-                    exceptionSerializer,
-                    partitionId,
-                    replicaOrInstanceId);
-
-                var methodStartTime = DateTime.Now;
-                Mock.Get(this.mockDiagnosticsSource).Setup(d => d.OnRequestResponseBegin()).Returns(methodStartTime);
-                Mock.Get(this.mockDiagnosticsSource).Setup(d => d.OnRequestResponseEnd(It.IsAny<DateTime>())).Verifiable();
-
+                // After creating SUT, we replace DiagnosticsSource with a mock, so we can verify diagnostics calls
                 var handlerType = typeof(FabricTransportMessageHandler);
                 var diagnosticsField = handlerType
                     .GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
                     .FirstOrDefault(f => f.FieldType == typeof(IDiagnosticsSource));
-                diagnosticsField?.SetValue(handler, mockDiagnosticsSource);
+                diagnosticsField?.SetValue(sut, mockDiagnosticsSource);
+            }
+
+            [Fact]
+            public async Task ShouldCallRequestResponseDiagnostics()
+            {
+                var methodStartTime = DateTime.Now;
+                Mock.Get(this.mockDiagnosticsSource).Setup(d => d.OnRequestResponseBegin()).Returns(methodStartTime);
 
                 try
                 {
-                    await handler.RequestResponseAsync(requestContext, fabricTransportMessage);
+                    await sut.RequestResponseAsync(requestContext, fabricTransportMessage);
                 }
                 catch
                 {
@@ -154,31 +144,14 @@ namespace Microsoft.ServiceFabric.Services.Remoting.Tests.V2.FabricTransport.Run
             }            
             
             [Fact]
-            public async Task RequestResponseAsync_ShouldCall_OnTransportMessageBeginEnd()
+            public async Task ShouldCallTransportMessageDiagnostics()
             {
-                var fabricTransportMessage = new FabricTransportMessage(new FabricTransportRequestHeader(Mock.Of<Stream>()), new FabricTransportRequestBody(Mock.Of<Stream>()));
-                var requestContext = new FabricTransportRequestContext(null, null);
-
-                var handler = new FabricTransportMessageHandler(
-                    mockRemotingMessageHandler,
-                    mockSerializerManager,
-                    exceptionSerializer,
-                    partitionId,
-                    replicaOrInstanceId);
-
                 var methodStartTime = DateTime.Now;
                 Mock.Get(this.mockDiagnosticsSource).Setup(d => d.OnCreateTransportMessageSerializationBegin()).Returns(methodStartTime);
-                Mock.Get(this.mockDiagnosticsSource).Setup(d => d.OnCreateTransportMessageSerializationEnd(It.IsAny<DateTime>())).Verifiable();
-
-                var handlerType = typeof(FabricTransportMessageHandler);
-                var diagnosticsField = handlerType
-                    .GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
-                    .FirstOrDefault(f => f.FieldType == typeof(IDiagnosticsSource));
-                diagnosticsField?.SetValue(handler, this.mockDiagnosticsSource);
 
                 try
                 {
-                    await handler.RequestResponseAsync(requestContext, fabricTransportMessage);
+                    await sut.RequestResponseAsync(requestContext, fabricTransportMessage);
                 }
                 catch
                 {
@@ -190,31 +163,14 @@ namespace Microsoft.ServiceFabric.Services.Remoting.Tests.V2.FabricTransport.Run
             }            
             
             [Fact]
-            public async Task RequestResponseAsync_ShouldCall_OnRemotingMessageBeginEnd()
+            public async Task ShouldCallRemotingMessageDiagnostics()
             {
-                var fabricTransportMessage = new FabricTransportMessage(new FabricTransportRequestHeader(Mock.Of<Stream>()), new FabricTransportRequestBody(Mock.Of<Stream>()));
-                var requestContext = new FabricTransportRequestContext(null, null);
-
-                var handler = new FabricTransportMessageHandler(
-                    mockRemotingMessageHandler,
-                    mockSerializerManager,
-                    exceptionSerializer,
-                    partitionId,
-                    replicaOrInstanceId);
-
                 var methodStartTime = DateTime.Now;
                 Mock.Get(this.mockDiagnosticsSource).Setup(d => d.OnRemotingRequestDeserializationBegin()).Returns(methodStartTime);
-                Mock.Get(this.mockDiagnosticsSource).Setup(d => d.OnRemotingRequestDeserializationEnd(It.IsAny<DateTime>())).Verifiable();
-
-                var handlerType = typeof(FabricTransportMessageHandler);
-                var diagnosticsField = handlerType
-                    .GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
-                    .FirstOrDefault(f => f.FieldType == typeof(IDiagnosticsSource));
-                diagnosticsField?.SetValue(handler, this.mockDiagnosticsSource);
 
                 try
                 {
-                    await handler.RequestResponseAsync(requestContext, fabricTransportMessage);
+                    await sut.RequestResponseAsync(requestContext, fabricTransportMessage);
                 }
                 catch
                 {
@@ -226,18 +182,8 @@ namespace Microsoft.ServiceFabric.Services.Remoting.Tests.V2.FabricTransport.Run
             }
         
             [Fact]
-            public async Task RequestResponseAsync_ShouldCall_InCorrectSequence()
+            public async Task ShouldCallDiagnostics_InCorrectSequence()
             {
-                var fabricTransportMessage = new FabricTransportMessage(new FabricTransportRequestHeader(Mock.Of<Stream>()), new FabricTransportRequestBody(Mock.Of<Stream>()));
-                var requestContext = new FabricTransportRequestContext(null, null);
-
-                var handler = new FabricTransportMessageHandler(
-                    mockRemotingMessageHandler,
-                    mockSerializerManager,
-                    exceptionSerializer,
-                    partitionId,
-                    replicaOrInstanceId);
-
                 var sequence = new MockSequence();
                 var methodStartTime = DateTime.Now;
                 var methodSerializationStartTime = methodStartTime.AddMinutes(1);
@@ -258,15 +204,9 @@ namespace Microsoft.ServiceFabric.Services.Remoting.Tests.V2.FabricTransport.Run
 
                 Mock.Get(this.mockDiagnosticsSource).InSequence(sequence).Setup(d => d.OnRequestResponseBegin()).Returns(methodStartTime).Callback(onRequestResponseBeginCallback);
 
-                var handlerType = typeof(FabricTransportMessageHandler);
-                var diagnosticsField = handlerType
-                    .GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
-                    .FirstOrDefault(f => f.FieldType == typeof(IDiagnosticsSource));
-                diagnosticsField?.SetValue(handler, this.mockDiagnosticsSource);
-
                 try
                 {
-                    await handler.RequestResponseAsync(requestContext, fabricTransportMessage);
+                    await sut.RequestResponseAsync(requestContext, fabricTransportMessage);
                 }
                 catch
                 {
