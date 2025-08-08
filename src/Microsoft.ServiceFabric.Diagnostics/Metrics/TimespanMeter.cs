e1dc162902410b77dceade0dd952c2eb1069261f
Ilija Grbić <ilijagrbi@microsoft.com>
2025-08-08 16:33:09 +0200

    Add TimespanMeter classes

---
+++ src/Microsoft.ServiceFabric.Diagnostics/Metrics/TimespanMeter.cs
@@ -0,0 +1,17 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using System;
+
+namespace Microsoft.ServiceFabric.Diagnostics.Metrics
+{
+    internal class TimespanMeter : IMeter<TimeSpan>
+    {
+        public void Record(TimeSpan value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Microsoft.ServiceFabric.Diagnostics/Metrics/TimespanMeter1D.cs
@@ -0,0 +1,16 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using System;
+namespace Microsoft.ServiceFabric.Diagnostics.Metrics
+{
+    internal class TimespanMeter1D : IMeter<TimeSpan>
+    {
+        public void Record(TimeSpan value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
