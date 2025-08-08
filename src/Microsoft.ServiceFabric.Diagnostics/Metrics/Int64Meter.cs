932238371011d7e34c6ad0364e05f7f2758fb970
Ilija Grbić <ilijagrbi@microsoft.com>
2025-08-08 16:30:57 +0200

    Initialize Int64Meter classes

---
+++ src/Microsoft.ServiceFabric.Diagnostics/Metrics/Int64Meter.cs
@@ -0,0 +1,17 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using System;
+
+namespace Microsoft.ServiceFabric.Diagnostics.Metrics
+{
+    internal class Int64Meter : IMeter<long>
+    {
+        public void Record(long value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
