5006c50457c5da95491412fabe3197be8afd2250
Ilija Grbić <ilijagrbi@microsoft.com>
2025-08-21 15:19:58 +0200

    Revert param reorder

--- src/Microsoft.ServiceFabric.Diagnostics/Metrics/Implementation/IFabricMeter.cs
+++ src/Microsoft.ServiceFabric.Diagnostics/Metrics/Implementation/IFabricMeter.cs
@@ -15,7 +15,7 @@ namespace Microsoft.ServiceFabric.Diagnostics.Metrics.Implementation
     interface IFabricMeter
     {
         [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
-        void Record(long value, uint count, [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPWStr)] string[] dimensionValues);
+        void Record(long value, [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPWStr)] string[] dimensionValues, uint count);
     }
 
 }
