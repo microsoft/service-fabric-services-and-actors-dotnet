cacbaef5a0ee3468061686ed228e0666a5e3245f
Ilija Grbić <ilijagrbi@microsoft.com>
2025-08-22 12:29:15 +0200

    Revert "Create UninstallSfSdkFromGac.ps1"
    This reverts commit 166dd95b53eb0f0a1810a4819a55f8e496d54958.

--- UninstallSfSdkFromGac.ps1
+++
@@ -1,7 +0,0 @@
-﻿$assemblies = "Microsoft.ServiceFabric.Data.Interfaces",
-              "Microsoft.ServiceFabric.Data.Interfaces.V2"
-
-foreach ($assembly in $assemblies)
-{
-    gacutil.exe /u $assembly
-}
