343ed98a8d29ec059946c0533467b6d203f47aca
Pavle Ciric <plaveongit@outlook.com>
2025-09-02 15:01:58 +0200

    Add reminder unregistering test

--- test/Microsoft.ServiceFabric.Actors.IntegrationTests/Reminders/ReminderRegistrationTest.cs
+++ test/Microsoft.ServiceFabric.Actors.IntegrationTests/Reminders/ReminderRegistrationTest.cs
@@ -56,6 +56,7 @@ namespace Microsoft.ServiceFabric.Actors
         interface ITestableActor : IActor
         {
             Task<IActorReminder> RegisterReminderAsync(string reminderName, byte[] state, TimeSpan dueTime, TimeSpan period);
+            Task UnregisterReminderAsync(IActorReminder reminder);
         }
 
         class TestableActor : Actor, ITestableActor, IRemindable
@@ -85,6 +86,11 @@ namespace Microsoft.ServiceFabric.Actors
             public new Task<IActorReminder> RegisterReminderAsync(string reminderName, byte[] state, TimeSpan dueTime, TimeSpan period)
             {
                 return base.RegisterReminderAsync(reminderName, state, dueTime, period);
+            }
+
+            public new Task UnregisterReminderAsync(IActorReminder reminder)
+            {
+                return base.UnregisterReminderAsync(reminder);
             }
         }
 
@@ -232,7 +238,7 @@ namespace Microsoft.ServiceFabric.Actors
                         byte[] fuzzyReminderState = UTF8Encoding.UTF8.GetBytes(fuzzy.String(Length.Between(5, 10)));
                         TimeSpan fuzzyDueTime = TimeSpan.FromSeconds(fuzzy.Int32().Between(1, 5));
                         TimeSpan fuzzyPeriod = TimeSpan.FromSeconds(fuzzy.Int32().Between(1, 5));
-
+                        
                         IActorReminder reminderResult = await testActor.RegisterReminderAsync(fuzzyReminderName, fuzzyReminderState, fuzzyDueTime, fuzzyPeriod);
                         return (ActorReminder)reminderResult;
                     };
@@ -260,6 +266,81 @@ namespace Microsoft.ServiceFabric.Actors
                     Assert.False(firstReminder.IsValid());
                     Assert.True(secondReminder.IsValid());
                 }
+            }
+        }
+
+        public class UnregisterReminderAsync : ActorIntegrationTest
+        {
+            [Fact]
+            public async Task ReminderIsInvalidatedAndDoesNotFireCallback()
+            {
+                IFuzz fuzzy = new RandomFuzz();
+
+                int reminderCallbackInvocationCounter = 0;
+                int callbackInvocationCounterBeforeUnregistering = 0;
+                int callbackInvocationCounterAfterUnregistering = 0;
+
+                ActorReminder reminderToUnregister = null;
+
+                string fuzzyReminderName = fuzzy.String(Length.Between(5, 10));
+                byte[] fuzzyReminderState = UTF8Encoding.UTF8.GetBytes(fuzzy.String(Length.Between(5, 10)));
+                TimeSpan fuzzyDueTime = TimeSpan.FromSeconds(fuzzy.Int32().Between(1, 5));
+                TimeSpan fuzzyPeriod = TimeSpan.FromSeconds(fuzzy.Int32().Between(1, 5));
+                TimeSpan allowedTimeVariation = TimeSpan.FromMilliseconds(100);
+
+                Func<ActorBase, CancellationToken, Task<ActorReminder>> registerActorReminder = async (actorBase, cancellationToken) =>
+                {
+                    var testActor = (ITestableActor)actorBase;
+
+                    IActorReminder reminderResult = await testActor.RegisterReminderAsync(fuzzyReminderName, fuzzyReminderState, fuzzyDueTime, fuzzyPeriod);
+                    return (ActorReminder)reminderResult;
+                };
+
+                Func<ActorBase, CancellationToken, Task<ActorReminder>> unregisterActorReminder = async (actorBase, cancellationToke) =>
+                {
+                    var testActor = (ITestableActor)actorBase;
+                    await testActor.UnregisterReminderAsync(reminderToUnregister);
+
+                    return reminderToUnregister;
+                };
+
+                Action<ReminderCallbackInfo> receiveReminderCallback = (reminderCallbackInfo) =>
+                {
+                    reminderCallbackInvocationCounter += 1;
+                };
+
+                Func<ActorService, ActorId, ActorBase> actorFactory = (actorService, actorId) => new TestableActor(actorService, actorId, receiveReminderCallback);
+
+                ActorService actorService = await GetActorService<TestableActor>(actorFactory);
+
+                ActorReminder reminderResult = await actorService.ActorManager.DispatchToActorAsync(
+                    actorId: new ActorId("TestableActor4"),
+                    actorMethodContext: new ActorMethodContext(),
+                    createIfRequired: true,
+                    registerActorReminder,
+                    callContext: "TestCallContext",
+                    timerCall: false,
+                    cancellationToken: new CancellationToken());
+
+                reminderToUnregister = reminderResult;
+                callbackInvocationCounterBeforeUnregistering = reminderCallbackInvocationCounter;
+
+                await actorService.ActorManager.DispatchToActorAsync(
+                    actorId: new ActorId("TestableActor4"),
+                    actorMethodContext: new ActorMethodContext(),
+                    createIfRequired: true,
+                    unregisterActorReminder,
+                    callContext: "TestCallContext",
+                    timerCall: false,
+                    cancellationToken: new CancellationToken());
+
+
+                await Task.Delay(fuzzyDueTime + fuzzyPeriod + allowedTimeVariation);
+
+                callbackInvocationCounterAfterUnregistering = reminderCallbackInvocationCounter;
+
+                Assert.False(reminderToUnregister.IsValid());
+                Assert.Equal(callbackInvocationCounterBeforeUnregistering, callbackInvocationCounterAfterUnregistering);
             }
         }
     }
