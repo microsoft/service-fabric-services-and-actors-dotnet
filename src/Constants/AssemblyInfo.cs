1911ce986598e908390ebfdb4eedb108d1d73977
Oleg Sych <olegsych@users.noreply.github.com>
2025-09-26 11:43:05 -0700

    Extract public and test keys into reusable constants
    - Create a new project, Microsoft.ServiceFabric.Constants to hold compile-time constants that should produce runtime-dependencies.
    - Move the Signature class from the TestFramework to the Constants project and rename it to AssemblyInfo, following the widely used convention established by the Visual Studio project templates.
    - Replace hard-coded public keys in all projects with the AssemblyInfo constants.

---
+++ src/Constants/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.ServiceFabric.Constants
+{
+    /// <summary>
+    /// Provides compile-time constants describing Service Fabric assemblies.
+    /// </summary>
+    /// <remarks>
+    /// This class is public to avoid having to list all Service Fabric assemblies using this project in <see cref="InternalsVisibleToAttribute"/>s.
+    /// </remarks>
+    public static class AssemblyInfo
+    {
+        /// <summary>
+        /// Public key of the strong name signature of the assemblies we publish externally.
+        /// </summary>
+        /// <remarks>
+        /// This key requires remote signing and projects using it produce delay-signed assemblies.
+        /// Delay-signed assemblies cannot be loaded by .NET Framework unless unless strong name
+        /// verification is disabled.
+        /// </remarks>
+        public const string PublicKey = ",PublicKey=0024000004800000940000000602000000240000525341310004000001000100b5fc90e7027f67871e773a8fde8938c81dd402ba65b9201d60593e96c492651e889cc13f1415ebb53fac1131ae0bd333c5ee6021672d9718ea31a8aebd0da0072f25d87dba6fc90ffd598ed4da35e44c398c454307e8e33b8426143daec9f596836f97c8f74750e5975c64e2189f45def46b2a2b1247adc3652bf5c308055da9";
+
+        /// <summary>
+        /// Public key of the strong name signature of the .NET Framework test assemblies.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// We don't use this key in this repository, only in the internal WindowsFabric repository.
+        /// There, we fully-sign the .NET Framework test- and some product-assemblies presumably to avoid
+        /// having to disable strong-name verification for each test assembly individually, or disabling
+        /// verification of all assemblies signed with the <see cref="PublicKey"/> because that includes
+        /// non-Service Fabric assemblies also signed by the same key by other product teams at Microsoft.
+        /// </para>
+        /// <para>
+        /// We don't use this key to build the .NET Core test assemblies, because .NET doesn't verify strong names of assemblies.
+        /// </para>
+        /// </remarks>
+        public const string TestKey = ",PublicKey=00240000048000009400000006020000002400005253413100040000010001003f8c902c8fe7ac83af7401b14c1bd103973b26dfafb2b77eda478a2539b979b56ce47f36336741b4ec52bbc51fecd51ba23810cec47070f3e29a2261a2d1d08e4b2b4b457beaa91460055f78cc89f21cd028377af0cc5e6c04699b6856a1e49d5fad3ef16d3c3d6010f40df0a7d6cc2ee11744b5cfb42e0f19a52b8a29dc31b0";
+    }
+}
