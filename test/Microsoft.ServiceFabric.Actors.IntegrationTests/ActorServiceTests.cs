48f0a08b61396feaf0452a50f485f3c4c7874433
Pavle Ciric <plaveongit@outlook.com>
2025-09-26 10:33:44 +0200

    Add additional tests for enumeration when no changes to reminder occur

--- test/Microsoft.ServiceFabric.Actors.IntegrationTests/ActorServiceTests.cs
+++ test/Microsoft.ServiceFabric.Actors.IntegrationTests/ActorServiceTests.cs
@@ -69,6 +69,7 @@ namespace Microsoft.ServiceFabric.Actors
             public class WhenReminderAreRegister : GetRemindersAsync
             {
                 protected readonly IActorStateProvider actorStateProviderWithReminders;
+                protected readonly List<ActorId> allActors;
                 protected readonly int numberOfActor;
                 protected readonly int numberOfReminderPerActor;
 
@@ -78,15 +79,18 @@ namespace Microsoft.ServiceFabric.Actors
                     numberOfActor = fuzzy.Int32().Between(5, 10);
                     numberOfReminderPerActor = fuzzy.Int32().Between(10, 20);
                     actorStateProviderWithReminders = new NullActorStateProvider();
+                    allActors = new List<ActorId>();
 
                     for (int i = 0; i < numberOfActor; i++)
                     {
                         for (int j = 0; j < numberOfReminderPerActor; j++)
                         {
+                            var actorId = new ActorId($"Actor_{i}");
                             var reminderMock = new Mock<IActorReminder>();
                             reminderMock.SetupGet(r => r.Name).Returns($"Reminder_{j}");
 
-                            actorStateProviderWithReminders.SaveReminderAsync(new ActorId($"Actor_{i}"), reminderMock.Object).Wait();
+                            allActors.Add(actorId);
+                            actorStateProviderWithReminders.SaveReminderAsync(actorId, reminderMock.Object).Wait();
                         }
                     }
                 }
@@ -111,9 +115,43 @@ namespace Microsoft.ServiceFabric.Actors
                     public class WhenActorIdIsGiven : WhenNoChangesAreMadeToTheRemindersBetweenResults
                     {
                         [Fact]
-                        public void ReturnsTheSameReminderWhichHaveBeenRegisteredForEachActor()
+                        public async Task ReturnsTheSameReminderWhichHaveBeenRegisteredForEachActor()
                         {
-                            Assert.True(true);
+                            IActorService actorService = await GetActorService<TestActor>(actorStateProvider: actorStateProviderWithReminders);
+
+                            foreach (ActorId actorId in allActors)
+                            {
+                                ContinuationToken continuationToken = null;
+                                var allQueriedActors = new List<ActorId>();
+                                var allQueriedRemindersPerActor = new List<ActorReminderState>();
+
+                                do
+                                {
+                                    var page = await actorService.GetRemindersAsync(actorId, continuationToken, default);
+                                    continuationToken = page.ContinuationToken;
+
+                                    foreach (var kvp in page.Items)
+                                    {
+                                        ActorId queriedActor = kvp.Key;
+                                        List<ActorReminderState> queriedReminderList = kvp.Value;
+
+                                        allQueriedActors.Add(queriedActor);
+                                        allQueriedRemindersPerActor.AddRange(queriedReminderList);
+                                    }
+                                }
+                                while (continuationToken != null);
+
+                                var actorsNotMatchingQuery = allQueriedActors.Where(aid => aid != actorId);
+                                var duplicateReminders = allQueriedRemindersPerActor
+                                    .GroupBy(r => r) // Group by ActorReminderState
+                                    .Where(g => g.Count() > 1) // Filter out group which have 1 or less ActorReminderStates
+                                    .SelectMany(g => g) // Flattens groups
+                                    .ToList();
+
+                                Assert.Equal(numberOfReminderPerActor, allQueriedRemindersPerActor.Count());
+                                Assert.Empty(actorsNotMatchingQuery);
+                                Assert.Empty(duplicateReminders);
+                            }
                         }
                     }
 
@@ -139,10 +177,7 @@ namespace Microsoft.ServiceFabric.Actors
                                     ActorId actorId = kvp.Key;
                                     List<ActorReminderState> reminderList = kvp.Value;
 
-                                    foreach (var reminderState in reminderList)
-                                    {
-                                        allReminders.Add(reminderState);
-                                    }
+                                    allReminders.AddRange(reminderList);
                                 }
                             }
                             while (continuationToken != null);
