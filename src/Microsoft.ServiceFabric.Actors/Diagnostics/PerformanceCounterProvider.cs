d5c8c9e7e9723d453359ae3297dd5596fa135162
Ilija Grbić <ilijagrbi@microsoft.com>
2025-10-09 14:32:10 +0200

    Remove unused methods in DiagnosticsEventManager

--- src/Microsoft.ServiceFabric.Actors/Diagnostics/PerformanceCounterProvider.cs
+++ src/Microsoft.ServiceFabric.Actors/Diagnostics/PerformanceCounterProvider.cs
@@ -175,8 +175,6 @@ namespace Microsoft.ServiceFabric.Actors.Diagnostics
             diagnosticsEventManager.OnActorRequestProcessingFinish += this.OnActorRequestProcessingFinish;
             diagnosticsEventManager.OnActorLockAcquired += this.OnActorLockAcquired;
             diagnosticsEventManager.OnActorLockReleased += this.OnActorLockReleased;
-            diagnosticsEventManager.OnActorRequestDeserializationFinish += this.OnActorRequestDeserializationFinish;
-            diagnosticsEventManager.OnActorResponseSerializationFinish += this.OnActorResponseSerializationFinish;
             diagnosticsEventManager.OnActorOnActivateAsyncFinish += this.OnActorOnActivateAsyncFinish;
             diagnosticsEventManager.OnLoadActorStateFinish += this.OnLoadActorStateFinish;
         }
