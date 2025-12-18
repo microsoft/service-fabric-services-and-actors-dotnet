cbcf061a3762b5040f8f285c607881db46f5f830
Ilija Grbić <ilijagrbi@microsoft.com>
2025-12-18 13:22:42 +0100

    Create AssemblyInfo.cs

---
+++ test/Microsoft.ServiceFabric.Actors.IntegrationTests/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using Xunit;
+
+// Run tests sequentially to prevent failures in tests that depend on global state.
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly)]
+[assembly: CaptureConsole]
