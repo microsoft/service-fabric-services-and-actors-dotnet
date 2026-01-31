677a393efbd5bb8ea485c2fd93dfc2cef5e04d09
Oleg Sych <olegsych@users.noreply.github.com>
2026-01-30 18:52:24 -0800

    Update init.cmd
    - Remove unnecessary runtimes already installed by the SDK package

--- init.cmd
+++ init.cmd
@@ -1,11 +1,7 @@
-: Run as admin to configure local development pre-requisites
-
 winget install Microsoft.DotNet.AspNetCore.8
 winget install Microsoft.DotNet.AspNetCore.9
-winget install Microsoft.DotNet.AspNetCore.10
 winget install Microsoft.DotNet.Runtime.8
 winget install Microsoft.DotNet.Runtime.9
-winget install Microsoft.DotNet.Runtime.10
 winget install Microsoft.DotNet.SDK.10
 
 pwsh SkipStrongName.ps1
