e841d8640968d0642c44f14ec1bfc6e9e0e40b57
Oleg Sych <olegsych@users.noreply.github.com>
2026-06-15 16:10:58 -0700

    squash! src/FabricTransport/Common/FabricTransportSettings.cs
    Address the following finding.
    
    ### LoadFrom/TryLoadFrom -- <param> descriptions missing introductory articles
    
    Per docs.instructions.md, <param> descriptions must begin with an
    introductory article. Prefixed "The " to the sectionName and
    configPackageName <param> descriptions in LoadFrom and TryLoadFrom.

--- src/FabricTransport/Common/FabricTransportSettings.cs
+++ src/FabricTransport/Common/FabricTransportSettings.cs
@@ -115,10 +115,10 @@ namespace Microsoft.ServiceFabric.FabricTransport
         /// Configuration File can be specified using the filePath or using the name of the configuration package specified in the service manifest .
         /// It will first try to load config using configPackageName . if configPackageName is not specified then try to load  from filePath.
         /// </summary>
-        /// <param name="sectionName">Name of the section within the configuration file.</param>
+        /// <param name="sectionName">The name of the section within the configuration file.</param>
         /// <param name="filepath"> Full path of the file where the settings will be loaded from. 
         ///  If not specified , it will first try to load from default Config Package"Config" , if not found then load from Settings "ClientExeName.Settings.xml" present in Client Exe directory. </param>
-        ///  <param name="configPackageName"> Name of the configuration package.If its null or empty,it will check for file in filePath</param>
+        ///  <param name="configPackageName"> The name of the configuration package.If its null or empty,it will check for file in filePath</param>
         /// <returns>The settings loaded from the specified configuration section.</returns>
         /// <remarks>
         /// The following are the parameter names that should be provided in the configuration file,to be recognizable by service fabric to load the transport settings.
@@ -201,10 +201,10 @@ namespace Microsoft.ServiceFabric.FabricTransport
         /// Configuration File can be specified using the filePath or using the name of the configuration package specified in the service manifest .
         /// It will first try to load config using configPackageName . if configPackageName is not specified then try to load  from filePath.
         /// </summary>
-        /// <param name="sectionName">Name of the section within the configuration file. If not found section in configuration file, it return false</param>
+        /// <param name="sectionName">The name of the section within the configuration file. If not found section in configuration file, it return false</param>
         /// <param name="filepath"> Full path of the file where the settings will be loaded from. 
         ///  If not specified , it will first try to load from default Config Package"Config" , if not found then load from Settings "ClientExeName.Settings.xml" present in Client Exe directory. </param>
-        ///  <param name="configPackageName"> Name of the configuration package. If its null or empty,it will check for file in filePath</param>
+        ///  <param name="configPackageName"> The name of the configuration package. If its null or empty,it will check for file in filePath</param>
         /// <param name="settings">When this method returns, contains the <see cref="FabricTransportSettings"/> loaded from configuration if the load succeeded, or <see langword="null"/> if it failed. This parameter is treated as uninitialized.</param>
         /// <returns><see langword="true"/> if the settings were loaded successfully from configuration; otherwise, <see langword="false"/>.</returns>
         /// <remarks>
