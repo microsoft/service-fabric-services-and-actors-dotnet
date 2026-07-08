5682b01fbf9eceba6af346186f4a0f16692a0cbe
Oleg Sych <olegsych@users.noreply.github.com>
2026-07-08 14:19:48 -0700

    src\Services.Wcf\Communication\Wcf\WcfRemoteExceptionInformation.cs
    Remove unnecessary usings.

--- src/Services.Wcf/Communication/Wcf/WcfRemoteExceptionInformation.cs
+++ src/Services.Wcf/Communication/Wcf/WcfRemoteExceptionInformation.cs
@@ -1,13 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
 
-using System;
-using System.Globalization;
-using System.IO;
-using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.Text;
-using System.Xml;
 
 namespace Microsoft.ServiceFabric.Services.Communication.Wcf;
 
