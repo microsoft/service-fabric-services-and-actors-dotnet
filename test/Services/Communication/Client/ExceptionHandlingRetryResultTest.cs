fa1a8f6d2f86bfaefbe9b50189618cc41d3b856b
Oleg Sych <olegsych@users.noreply.github.com>
2026-07-09 14:48:41 -0700

    test/Services/Communication/Client/ExceptionHandlingRetryResultTest.cs
    Implement unit tests for Microsoft.ServiceFabric.Services.Communication.Client.ExceptionHandlingRetryResult in src/Services/Communication/Client/ExceptionHandlingRetryResult.cs.
    
    Test Microsoft.ServiceFabric.Services.Communication.Client.ExceptionHandlingRetryResult in src/Services/Communication/Client/ExceptionHandlingRetryResult.cs. Bug fixes, testability improvements are out of scope. Restructure existing, eliminate redundant tests as needed.

---
+++ test/Services/Communication/Client/ExceptionHandlingRetryResultTest.cs
@@ -0,0 +1,112 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+
+using System;
+using Fuzzy;
+using Moq;
+using Xunit;
+
+namespace Microsoft.ServiceFabric.Services.Communication.Client;
+
+public abstract class ExceptionHandlingRetryResultTest
+{
+    // Constructor parameters
+    readonly Exception exception = new();
+    readonly string exceptionId = fuzzy.String();
+    readonly TimeSpan retryDelay = fuzzy.TimeSpan();
+    readonly OperationRetrySettings retrySettings;
+    readonly int maxRetryCount = fuzzy.Int32();
+
+    // Fixture
+    static readonly IFuzz fuzzy = new RandomFuzz(Environment.TickCount);
+    readonly Mock<IRetryPolicy> retryPolicy = new();
+
+    protected ExceptionHandlingRetryResultTest() =>
+        retrySettings = new OperationRetrySettings(retryPolicy.Object);
+
+    public sealed class Constructor_Exception_Boolean_OperationRetrySettings_Int32 : ExceptionHandlingRetryResultTest
+    {
+        [Theory, InlineData(true), InlineData(false)]
+        public void InitializesProperties(bool isTransient)
+        {
+            TimeSpan expectedDelay = fuzzy.TimeSpan();
+            _ = retryPolicy.Setup(_ => _.GetNextRetryDelay(It.Is<RetryDelayParameters>(p => p.RetryAttempt == 0 && p.IsTransient == isTransient))).Returns(expectedDelay);
+
+            var sut = new ExceptionHandlingRetryResult(exception, isTransient, retrySettings, maxRetryCount);
+
+            Assert.Equal(exception.GetType().FullName, sut.ExceptionId);
+            Assert.Equal(isTransient, sut.IsTransient);
+            Assert.Equal(expectedDelay, sut.RetryDelay);
+            Assert.Equal(maxRetryCount, sut.MaxRetryCount);
+        }
+    }
+
+    public sealed class Constructor_Exception_Boolean_TimeSpan_Int32 : ExceptionHandlingRetryResultTest
+    {
+        [Theory, InlineData(true), InlineData(false)]
+        public void InitializesProperties(bool isTransient)
+        {
+            var sut = new ExceptionHandlingRetryResult(exception, isTransient, retryDelay, maxRetryCount);
+
+            Assert.Equal(exception.GetType().FullName, sut.ExceptionId);
+            Assert.Equal(isTransient, sut.IsTransient);
+            Assert.Equal(retryDelay, sut.RetryDelay);
+            Assert.Equal(maxRetryCount, sut.MaxRetryCount);
+        }
+    }
+
+    public sealed class Constructor_String_Boolean_OperationRetrySettings_Int32 : ExceptionHandlingRetryResultTest
+    {
+        [Theory, InlineData(true), InlineData(false)]
+        public void InitializesProperties(bool isTransient)
+        {
+            TimeSpan expectedDelay = fuzzy.TimeSpan();
+            _ = retryPolicy.Setup(_ => _.GetNextRetryDelay(It.Is<RetryDelayParameters>(p => p.RetryAttempt == 0 && p.IsTransient == isTransient))).Returns(expectedDelay);
+
+            var sut = new ExceptionHandlingRetryResult(exceptionId, isTransient, retrySettings, maxRetryCount);
+
+            Assert.Same(exceptionId, sut.ExceptionId);
+            Assert.Equal(isTransient, sut.IsTransient);
+            Assert.Equal(expectedDelay, sut.RetryDelay);
+            Assert.Equal(maxRetryCount, sut.MaxRetryCount);
+        }
+    }
+
+    public sealed class Constructor_String_Boolean_TimeSpan_Int32 : ExceptionHandlingRetryResultTest
+    {
+        [Theory, InlineData(true), InlineData(false)]
+        public void InitializesProperties(bool isTransient)
+        {
+            var sut = new ExceptionHandlingRetryResult(exceptionId, isTransient, retryDelay, maxRetryCount);
+
+            Assert.Same(exceptionId, sut.ExceptionId);
+            Assert.Equal(isTransient, sut.IsTransient);
+            Assert.Equal(retryDelay, sut.RetryDelay);
+            Assert.Equal(maxRetryCount, sut.MaxRetryCount);
+        }
+    }
+
+    public sealed class GetRetryDelay : ExceptionHandlingRetryResultTest
+    {
+        // Method parameters
+        readonly int retryAttempt = fuzzy.Int32();
+
+        [Fact]
+        public void ReturnsRetryDelayWhenRetrySettingsWasNotProvided()
+        {
+            var sut = new ExceptionHandlingRetryResult(exceptionId, fuzzy.Boolean(), retryDelay, maxRetryCount);
+            Assert.Equal(retryDelay, sut.GetRetryDelay(retryAttempt));
+        }
+
+        [Theory, InlineData(true), InlineData(false)]
+        public void ComputesRetryDelayFromRetryPolicyWhenRetrySettingsWasProvided(bool isTransient)
+        {
+            TimeSpan expected = fuzzy.TimeSpan();
+            _ = retryPolicy.Setup(_ => _.GetNextRetryDelay(It.Is<RetryDelayParameters>(p => p.RetryAttempt == retryAttempt && p.IsTransient == isTransient))).Returns(expected);
+
+            var sut = new ExceptionHandlingRetryResult(exceptionId, isTransient, retrySettings, maxRetryCount);
+
+            Assert.Equal(expected, sut.GetRetryDelay(retryAttempt));
+        }
+    }
+}
